d95ffdf0c3f9e8b3038a8b9b9ea905a4d7e07226
Jonathan Cortoppassi <106403752+cortoppassi@users.noreply.github.com>
2022-11-18 21:32:52 -0300

    Loop For

---
+++ caçaLetrasComLoop/script.js
@@ -0,0 +1,15 @@
+const chamar = function() {
+
+    const frase = document.getElementById('frase').value;
+    const letra = document.getElementById('letra').value;
+
+    for (var i = 0; i < frase.length; i++) {
+        // console.log(frase[i]);
+
+        if(frase[i] == letra){
+         console.log(letra)
+
+        }
+    }
+}
+//Treinando conceitos basicos, Loop For!
